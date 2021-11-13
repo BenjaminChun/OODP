cb3cff47ae11a86195b079e3a8adaf52dce8f76a
Tang Han Kang <58020827+tanghankang@users.noreply.github.com>
2021-11-13 23:38:38 +0800

    Create IncorrectTableIDException.java

---
+++ IncorrectTableIDException.java
@@ -0,0 +1,8 @@
+public class IncorrectTableIDException extends Exception{
+    public IncorrectTableIDException(){
+        super("Table ID cannot be found in List!");
+    }
+    public IncorrectTableIDException(String message){
+        super(message);
+    }
+}
