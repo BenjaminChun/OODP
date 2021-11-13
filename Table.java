7edff049792ce7705c18097f668ce29196d80be3
behmingjun <58068400+behmingjun@users.noreply.github.com>
2021-11-14 02:43:33 +0800

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,9 +1,37 @@
+/**
+ * Represents a Table in the restaurant.
+ * Table can have different number of seats,
+ * status and ID.
+ * @author Benjamin Chun Wei Jie
+ * @version 14
+ * @since 2021-11-13
+ */
 public class Table {
 
+	/**
+	 * The seating capacity of the Table
+	 */
 	private int seatingCapacity;
+
+	/**
+	 * The status of the Table (AVAILABLE, 
+	 * OCCUPIED, RESERVED).
+	 */
 	private Status status;
+
+	/**
+	 * The ID of the table.
+	 */
 	private int tableID;
 	
+	/**
+	 * Creates a Table object.
+	 * The table can only have 2/4/6/8/10 seats.
+	 * Table status can only be chosen from the Status class.
+	 * @param tableID 
+	 * @param seatingCapacity
+	 * @param status
+	 */
 	public Table(int tableID, int seatingCapacity, Status status) {
 		this.tableID = tableID;
 		this.seatingCapacity = seatingCapacity;
