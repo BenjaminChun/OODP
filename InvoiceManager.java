5204f6290afd7277d052cd7db8d8f0d8e44e5450
poweredbysprite <88825215+poweredbysprite@users.noreply.github.com>
2021-11-14 13:31:44 +0800

    Update InvoiceManager.java

--- InvoiceManager.java
+++ InvoiceManager.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Represents the Invoice Manager in a restaurant.
+ * Represents a Invoice Manager in a restaurant.
  * @author Tan Zheng Kai
  * @version 1.0
  * @since 2021-11-13
