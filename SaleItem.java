7404678fe70110ab85e4bb8b5e1a9ba82bc51594
poweredbysprite <88825215+poweredbysprite@users.noreply.github.com>
2021-11-14 16:06:02 +0800

    Update SaleItem.java

--- SaleItem.java
+++ SaleItem.java
@@ -1,11 +1,24 @@
+/**
+ * Represents the Invoice Manager in a restaurant.
+ * @author Tan Zheng Kai
+ * @version 1.0
+ * @since 2021-11-13
+ */
 public class SaleItem {
 
+	/**
+	 * The order item of this Sale Item.
+	 */
 	private OrderItem orderItem;
+	/**
+	 * The quantity of this Sale Item.
+	 */
 	private int quantity;
 
 	/**
-	 * 
-	 * @param menuItem
+	 * Creates a sale item with a given quantity.
+	 * @param orderItem Order Item of this Sale Item.
+	 * @param quantity Quantity of this Sale Item.
 	 */
 	public SaleItem(OrderItem orderItem , int quantity) {
 		this.orderItem = orderItem;
@@ -14,30 +27,43 @@ public class SaleItem {
 		//throw new UnsupportedOperationException();
 	}
 
+	/**
+	 * Gets the order item of this sale item.
+	 * @return the order item of this sale item.
+	 */
 	public OrderItem getOrderItem() {
 		return this.orderItem;
 	}
 
 	/**
-	 * 
+	 * Changes the order item of this sale item.
 	 * @param orderItem
 	 */
 	public void setOrderItem(OrderItem orderItem) {
 		this.orderItem = orderItem;
 	}
 
+	/**
+	 * Gets the quantity of this sale item.
+	 * @return the quantity of this sale item.
+	 */
 	public int getQuantity() {
 		return this.quantity;
 	}
 
 	/**
-	 * 
-	 * @param quantity
+	 * Changes the quantity of this sale item.
+	 * @param quantity The quantity of this sale item.
 	 */
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
 
+	/**
+	 * Increase the quantity by a specified value.
+	 * @param addition The specified value to be added.
+	 * @param quantity The quantity of this sale value.
+	 */
     public void incrementQuantity(int addition) {
 		this.quantity += addition;
     }
