47575bf79ab9c9f1ac21154b8bc010ead0211e02
poweredbysprite <88825215+poweredbysprite@users.noreply.github.com>
2021-11-14 22:18:54 +0800

    Update IncorrectTableIDException.java

--- IncorrectTableIDException.java
+++ IncorrectTableIDException.java
@@ -5,9 +5,16 @@
  * @since 2021-11-14
  */
 public class IncorrectTableIDException extends Exception{
-    public IncorrectTableIDException(){
+    /*
+     * Empty Constructor
+     */
+	public IncorrectTableIDException(){
         super("Table ID cannot be found in List!");
     }
+	/*
+	 * Creates an IncorrectTableIDException with message.
+	 * @param message to be produced when Exception encountered.
+	 */
     public IncorrectTableIDException(String message){
         super(message);
     }
